1909303ced4d22083d4b394357713c177c46efb1
genkidesu <tysaunders1@hotmail.com>
2021-03-04 21:57:38 +1100

    removed 1 buggy function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,29 @@
 // There is a function to alert user that some buttons are not working yet
 // another function to unhide contact details when a button is clicked
 
-$("#contactButton").on('click', function(){
-    alert('not ready yet');
-  });
+$("#contactButton").on('click', function () {
+  alert('not ready yet');
+});
 
-$(".sMain").on('click', function(){
-    alert('not ready yet');
-  });
+$(".sMain").on('click', function () {
+  alert('not ready yet');
+});
 
-$(".sOther").on('click', function(){
-    alert('not ready yet');
-  });
-  
-$(".seeDetails").on('click', function(){
-    $(".seeDetails").remove();
-    $(".hide").css("display", "inline-block");
-   });
+$(".sOther").on('click', function () {
+  alert('not ready yet');
+});
+
+$(".seeDetails").on('click', function () {
+  $(".seeDetails").remove();
+  $(".hide").css("display", "inline-block");
+});
 
 // Added below as the menu button wasnt working when visiting on phone  
-$(".drop").on('click', function(){
+$(".drop").on('click', function () {
   $(".dropped").css("display", "flex");
- });
+});
 
- $(".drop").hover(function(){
-  $('.dropped').fadeIn();
- });
-
- $(window).scroll(function() {
+$(window).scroll(function () {
   if ($(this).scrollTop() > 0) {
     $('.dropped').fadeOut();
   }
